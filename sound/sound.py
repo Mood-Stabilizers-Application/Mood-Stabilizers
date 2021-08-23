72f2e8f7077423edc3c5477fb761a8adb91e39b0
MOH <mohammadnoormjk1998@gmail.com>
2021-08-23 19:25:23 +0300

    add uint test for playsound funand modify code for it and modify data(songs)

--- sound/sound.py
+++ sound/sound.py
@@ -1,10 +1,7 @@
 from playsound import playsound
      
 def play_sound(mood):
-    
-    count =1
-    while count < 3:
-     playsound(f'//wsl$/Ubuntu/home/mohmmadnoorjebreen/project-mid/sound/music/{mood}/{mood}{count}.mp3')
-     count+=1
+    playsound(f'sound/music/{mood}/{mood}.mp3')
+    return f'sound/music/{mood}/{mood}.mp3'
 
 play_sound('sad')
